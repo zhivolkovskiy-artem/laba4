b84f59f4bbe6d235f8ada5d001474eb742a73568
zhivolkovskiy-artem <heartkilla66@gmail.com>
2015-10-04 22:48:06 +0300

    added 2nd problem

---
+++ laba4_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+#define MAXSIZE 100
+
+using namespace std;
+
+int caluclator(char *str)
+{
+	int size = strlen(str), sum = 0, i = 0;
+	while (i < size)
+	{
+		int j = i;
+		char *number = new char[MAXSIZE];
+		while (isdigit(str[j]))
+		{
+			j++;
+		}
+		for (int k = 0; k < j - i; k++)
+		{
+			number[k] = str[i + k];
+		}
+		if (str[i - 1] == '-') sum -= atoi(number);
+		else sum += atoi(number);
+		i += j - i + 1;
+		delete [] number;
+	}
+	
+	return sum;
+}
+
+int main()
+{
+	return 0;
+}
