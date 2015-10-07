9f90ae3b3eea7f99eef898341be1bd50b28ac8e2
KostyaKulak <kkulak24@gmail.com>
2015-10-07 19:39:31 +0300

    Update laba4_2.cpp

--- laba4_2.cpp
+++ laba4_2.cpp
@@ -21,14 +21,30 @@ int calculator(char *str)
 		if (str[i - 1] == '-') sum -= atoi(number);
 		else sum += atoi(number);
 		i += j - i + 1;
-		delete [] number;
+		delete[] number;
 	}
-	
+
 	return sum;
 }
 
-int main()
+void assert(char* s, int myanswer)
 {
-	
+	int ans = calculator(s);
+	if (ans == myanswer)
+		cout << "OK" << endl;
+	else
+		cout << "FAIL" << endl;
+}
+int main() {
+	char s1[] = "-43+3-1-11";
+	assert(s1, -52);
+	char s2[] = "324+36-3583-6+13";
+	assert(s2, -3216);
+	char s3[] = "-50182-123+1+0-1230";
+	assert(s3, -51534);
+	char s4[] = "276+297";
+	assert(s4, 573);
+	char s5[] = "123+321";
+	assert(s5, 444);
 	return 0;
 }
